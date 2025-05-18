2d174f022ea704524a50530d3db6b251cc7caf89
Patrick Zhao <patrick.zhao1989@outlook.com>
2025-05-18 14:44:06 +1000

    initial commit - create the boilerplate of backend .net web api

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jr-devops-demo
 This repo is for JR DevOps lecture 1 demo
+
+
+# Overview 
+
+We are going to build and deploy a web app with frontend in React and backend in .net
---
+++ backend/Controllers/HelloWorld.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+// create me a hello world controller as API controller
+
+[ApiController]
+[Route("api/[controller]")]
+public class HelloWorldController : ControllerBase
+{
+	[HttpGet]
+	public IActionResult Get()
+	{
+		return Ok("Hello JR student!");
+	}
+}
